580da90f1d8fe97f2b2b3b8f4da609a5dceb860d
Jimmy Lin <ex@example.com>
2020-10-04 17:13:52 -0400

    initial table for course planner

---
+++ src/components/CoursePlanner.vue
@@ -0,0 +1,45 @@
+
+<template>
+  <v-data-table
+    v-model="selected"
+    :headers="headers"
+    :items="courses"
+    :single-select="singleSelect"
+    item-key="name"
+    show-select
+    class="elevation-1"
+  >
+    <template v-slot:top>
+    </template>
+  </v-data-table>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      allCourses: ["course1", "course2"],
+      allMajors: ["major1", "major2"],
+
+      singleSelect: false,
+      selected: [],
+      headers: [
+        { text: "course", value: "name"},
+        { text: "major1", value: "major1"},
+        { text: "major2", value: "major2"},
+      ],
+      courses: [
+        {
+          name: "course1",
+          major1: "R",
+        },
+        {
+          name: "course2",
+          major1: "R",
+          major2: "O",
+        },
+      ]
+    };
+  }
+};
+</script>
