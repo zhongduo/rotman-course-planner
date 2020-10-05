dbca17b8a543d8648169d9e830b6f4fe8972888e
Jimmy Lin <ex@example.com>
2020-10-04 20:46:59 -0400

    add Major.vue

--- src/components/CoursePlanner.vue
+++ src/components/CoursePlanner.vue
@@ -1,45 +1,197 @@
 
 <template>
-  <v-data-table
-    v-model="selected"
-    :headers="headers"
-    :items="courses"
-    :single-select="singleSelect"
-    item-key="name"
-    show-select
-    class="elevation-1"
-  >
-    <template v-slot:top>
-    </template>
-  </v-data-table>
+  <div>
+    <v-data-table
+      v-model="selected"
+      :headers="headers"
+      :items="courses"
+      :single-select="singleSelect"
+      item-key="name"
+      show-select
+      class="elevation-1"
+      disable-pagination
+    >
+      <template v-slot:top></template>
+    </v-data-table>
+
+  <v-flex v-for="(item, i) in requirements" :key="i">
+    <Major :selected="selectedList" :requirement="item"></Major>
+  </v-flex>
+
+  </div>
 </template>
 
 <script>
+import Major from "./Major"
+
 export default {
   data() {
     return {
-      allCourses: ["course1", "course2"],
-      allMajors: ["major1", "major2"],
+      allCourses: [],
+      allMajors: [],
 
       singleSelect: false,
       selected: [],
-      headers: [
-        { text: "course", value: "name"},
-        { text: "major1", value: "major1"},
-        { text: "major2", value: "major2"},
+      headers: [/*
+        { text: "course", value: "name" },
+        { text: "major1", value: "major1" },
+        { text: "major2", value: "major2" }*/
       ],
       courses: [
-        {
+        /*{
           name: "course1",
-          major1: "R",
+          major1: "R"
         },
         {
           name: "course2",
           major1: "R",
-          major2: "O",
+          major2: "O"
+        }*/
+      ],
+
+      requirements: [
+        {
+          name: "Brand Management",
+          core: [
+            "Marketing Strategy",
+            "Consumer Behaviour",
+            "Strategic Marketing Communication",
+            "Marketing Research",
+            "Sales Management",
+            "Branding",
+            "Pricing",
+            "One to One Marketing",
+            "Digital Marketing",
+            "Marketing and Behavioural Economics",
+            "Creativity and Business Innovation"
+          ],
+          optional: ["Corporate Strategy", "Financial Management"],
+          coreRequired: 4,
+          optionalRequired: 0,
+          totalRequired: 4
         },
+        {
+          name: "Business Design",
+          core: [
+            "Business Design Fundamentals",
+            "Business Design Practicum",
+            "Creativity for Business Innovation OR Global Practicum: Design-Led Innovation",
+            "Design Research and Data Storytelling OR Service Design: Innovating Service-Based Organizations"
+          ],
+          optional: [],
+          coreRequired: 4,
+          optionalRequired: 0,
+          totalRequired: 4
+        },
+        {
+          name: "Consulting",
+          core: [
+            "Management Consulting",
+            "Healthcare Consulting",
+            "Management Consulting Practicum",
+            "Strategic Change and Implementation"
+          ],
+          optional: [
+            "Corporation 360°",
+            "Communications Strategy",
+            "Financial Statement Analysis",
+            "Business Analysis and Valuation",
+            "Corporate Strategy",
+            "Supply Chain Management",
+            "Operations Management Strategy",
+            "Analytics and Operations Consulting",
+            "Organization Design",
+            "Aligning People and Strategy",
+            "Catastrophic Failure in Organizations",
+            "Strategic Networks",
+            "Business Problem Solving: A Model-Based Approach"
+          ],
+          coreRequired: 1,
+          optionalRequired: 2,
+          totalRequired: 4
+        },
+        {
+          name: "Financial Reporting and Analysis",
+          core: [
+            "Taxation and Decision Making",
+            "Financial Statement Analysis",
+            "Financial Distress and Insolvency",
+            "Business Analysis and Valuation"
+          ],
+          optional: [
+            "Corporate Strategy",
+            "Corporate Financing",
+            "Financial Management",
+            "Financial Institutions and Capital Markets"
+          ],
+          coreRequired: 3,
+          optionalRequired: 0,
+          totalRequired: 3
+        }
       ]
     };
+  },
+  components: {
+    Major
+  },
+computed: {
+selectedList: function() {
+  var l = []
+  for (var c of this.selected) {
+    l.push(c.name)
+  }
+  return l
+}
+},
+  methods: {
+    genTable: function() {
+      var m, i, c, req
+      // gen header
+      this.headers.push({ text: "course", value: "name" });
+      i = 0;
+      for (m of this.allMajors) {
+        this.headers.push({ text: m, value: "m" + i });
+        i++;
+      }
+      for (c of this.allCourses) {
+        var courseDef = {
+          name: c
+        };
+        i = 0
+        for (m of this.allMajors) {
+          req = this.requirements.find((element) => (element.name == m))
+          if (req.core.includes(c)) {
+            courseDef["m"+i] = "R" 
+          } else if (req.optional.includes(c)) {
+            courseDef["m"+i] = "O"
+          }
+          i++
+        }
+        this.courses.push(courseDef);
+      }
+    },
+    genData: function() {
+      var req, c;
+      for (req of this.requirements) {
+        this.allMajors.push(req.name);
+        for (c of req.core) {
+          if (!this.allCourses.includes(c)) {
+            this.allCourses.push(c);
+          }
+        }
+        for (c of req.optional) {
+          if (!this.allCourses.includes(c)) {
+            this.allCourses.push(c);
+          }
+        }
+      }
+      this.allMajors.sort();
+      this.allCourses.sort();
+      this.genTable()
+    }
+  },
+  beforeMount() {
+    this.genData();
   }
 };
 </script>
---
+++ src/components/Major.vue
@@ -0,0 +1,107 @@
+<template>
+  <v-card class="mx-auto" max-width="344" outlined>
+    <v-list-item>
+      <v-list-item-avatar color="grey"></v-list-item-avatar>
+      <v-list-item-content>
+        <v-list-item-title class="headline" v-text="requirement.name"></v-list-item-title>
+      </v-list-item-content>
+    </v-list-item>
+
+    <v-list subheader flat>
+      <v-subheader>Core</v-subheader>
+      <v-list-item-group multiple v-for="(item, i) in requirement.core" :key="i">
+        <v-list-item>
+          <v-list-item-action>
+            <v-checkbox :input-value="selected.includes(item)" color="primary"></v-checkbox>
+          </v-list-item-action>
+
+          <v-list-item-content>
+            <v-list-item-title v-text="item"></v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list-item-group>
+    </v-list>
+
+    <v-list subheader flat>
+      <v-subheader>Optional</v-subheader>
+      <v-list-item-group multiple v-for="(item, i) in requirement.optional" :key="i">
+        <v-list-item>
+          <v-list-item-action>
+            <v-checkbox :input-value="selected.includes(item)" color="primary"></v-checkbox>
+          </v-list-item-action>
+
+          <v-list-item-content>
+            <v-list-item-title v-text="item"></v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list-item-group>
+    </v-list>
+
+    <v-list subheader flat>
+      <v-subheader>Summary</v-subheader>
+      <v-list-item-group multiple>
+        <v-list-item>
+            <v-checkbox :input-value="requirement.coreRequired <= numCore" color="primary"></v-checkbox>
+          <v-list-item-content>Core: required {{requirement.coreRequired}}, get {{numCore}}</v-list-item-content>
+        </v-list-item>
+        <v-list-item>
+            <v-checkbox :input-value="requirement.optionalRequired <= numOption" color="primary"></v-checkbox>
+          <v-list-item-content>Optional: required {{requirement.optionalRequired}}, get {{numOption}}</v-list-item-content>
+        </v-list-item>
+        <v-list-item>
+            <v-checkbox :input-value="requirement.totalRequired <= (numOption + numCore)" color="primary"></v-checkbox>
+          <v-list-item-content>Total: required {{requirement.totalRequired}}, get {{numOption + numCore}}</v-list-item-content>
+        </v-list-item>
+      </v-list-item-group>
+    </v-list>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: {
+    selected: {
+      type: Array,
+      required: true
+    },
+    requirement: {
+      type: Object,
+      required: true
+    }
+  },
+  computed: {
+    numCore: function() {
+      var n = 0;
+      for (var c of this.selected) {
+        if (this.requirement.core.includes(c)) {
+          n++;
+        }
+      }
+      return n;
+    },
+    numOption: function() {
+      var n = 0;
+      for (var c of this.selected) {
+        if (this.requirement.optional.includes(c)) {
+          n++;
+        }
+      }
+      return n;
+    }
+  },
+
+  methods: {
+    isSelected: function(course) {
+      for (var c of this.selected) {
+        if (c.name == course) {
+          return true;
+        }
+      }
+      return false;
+    }
+  },
+  beforeMount() {
+    //console.log(this.requirement);
+  }
+};
+</script>
